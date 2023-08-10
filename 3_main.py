984b8613f75cc8925fe18a06e495b1807c43a5c5
Jon <jonathancirillo@outlook.com>
2023-08-10 10:46:14 -0400

    fixed missing category on post request

--- 3_main.py
+++ 3_main.py
@@ -4,7 +4,7 @@ print("Adding an item:")
 print(
     requests.post(
         "http://127.0.0.1:8000/",
-        json={"name": "Screwdriver", "price": 3.99, "count": 10, "id": 4},
+        json={"name": "Screwdriver", "price": 3.99, "count": 10, "id": 4, "category": "tools"},
     ).json()
 )
 print(requests.get("http://127.0.0.1:8000/").json())
